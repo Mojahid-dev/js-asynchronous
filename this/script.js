a4dbbfa6add847eabcc8e6a5d8fea7241ac3f499
Md Mojahid <mojahid.dev.sihf@gmail.com>
2026-03-05 10:25:35 +0530

    this project

--- this/script.js
+++ this/script.js
@@ -1,17 +1,71 @@
 let form = document.querySelector("form");
+let userName = document.querySelector("#name");
+let bio = document.querySelector("#bio");
+let role = document.querySelector("#role");
+let photo = document.querySelector("#photo");
 
 
 const userManager = {
     users: [],
-    addUser: function () {},
-    removeUsr: function () {},
+    addUser: function () {
+        this.users.push({
+            userName: userName.value,
+            bio: bio.value,
+            role: role.value,
+            photo: photo.value,
+        });
+
+        form.reset();
+        this.renderUi();
+    },
+
+    removeUser: function () { },
     init: function () {
         form.addEventListener("submit", this.submitForm.bind(this));
     },
     submitForm: function (e) {
         e.preventDefault();
-        console.log("Form submitted succesfully");
-    }
+        this.addUser();
+    },
+
+    renderUi: function () {
+        this.users.forEach(function (user) {
+            // create main div
+            const card = document.createElement("div");
+            card.className = "bg-gray-800 border border-gray-700 rounded-xl p-6 text-center shadow";
+
+            // create image
+            const img = document.createElement("img");
+            img.src = user.photo;
+            img.className = "w-20 h-20 rounded-full mx-auto mb-4";
+            card.appendChild(img);
+
+            // create name
+            const name = document.createElement("h3");
+            name.className = "font-semibold text-lg";
+            name.textContent = user.userName;
+            card.appendChild(name);
+
+            // create role
+            const role = document.createElement("p");
+            role.className = "text-blue-400 text-sm";
+            role.textContent = user.role;
+            card.appendChild(role);
+
+            // create bio
+            const bio = document.createElement("p");
+            bio.className = "text-gray-400 text-sm mt-2";
+            bio.textContent = user.bio;
+            card.appendChild(bio);
+
+            // append elements
+
+            // add card to page
+            document.querySelector(".users").appendChild(card);
+        })
+    },
+
+
 }
 
 userManager.init();
